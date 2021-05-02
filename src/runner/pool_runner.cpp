12ddfb6743a5e71a488df598a37ab484c6719f9e
Nils Krüger <36828049+n-krueger@users.noreply.github.com>
2021-05-02 18:28:36 +0200

    Use hardware concurrency for FaasletPool size

--- src/runner/pool_runner.cpp
+++ src/runner/pool_runner.cpp
@@ -1,3 +1,5 @@
+#include <thread>
+
 #include <faabric/util/logging.h>
 
 #include <faaslet/FaasletPool.h>
@@ -12,9 +14,18 @@ int main()
 {
     const std::shared_ptr<spdlog::logger>& logger = faabric::util::getLogger();
 
+    // Determine maximum concurrency on this node, int cast because FaasletPool constructor
+    // requires int. If cast overflows or the number of supported threads is undefined we
+    // set them to 0 to suppor the default MPI world size.
+    int nAvailableThreads = (int) std::thread::hardware_concurrency();
+    if (nAvailableThreads <= 0) {
+        nAvailableThreads = 5;
+    }
+
     // Start the worker pool
     logger->info("Starting faaslet pool in the background");
-    FaasletPool p(5);
+    logger->info("Faaslet pool size is " + std::to_string(nAvailableThreads));
+    FaasletPool p(nAvailableThreads);
     FaabricMain w(p);
     w.startBackground();
 
